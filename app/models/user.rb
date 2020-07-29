3711b50edde8e793629c29ddedd7b94dd36eb787
chinweokwu <paulcmorah@gmail.com>
2020-07-29 12:12:01 +0100

    added validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,5 @@
 class User < ApplicationRecord
+    validates :username, length: { minimum: 2 }
+    validates :email, format: { with: /^[A-Za-z0-9+_.-]+@[A-Za-z0-9.-]+$/ }
+    validates :password, length: { in: 6..20 }
 end
